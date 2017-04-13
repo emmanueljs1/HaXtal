c4d41caa3a1fa24ec52662b3f7bb3af859deeffa
Kyle Rosenbluth <kyle.rosenbluth@gmail.com>
2017-04-13 12:39:13 -0400

    Add gloss and basic starter picture

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
 import Lib
+import Graphics.Gloss
+
+window :: Display
+window = InWindow "Nice Window" (200, 200) (10, 10)
+
+background :: Color
+background = white
+
+drawing :: Picture
+drawing = circle 80
 
 main :: IO ()
-main = someFunc
+main = display window background drawing
