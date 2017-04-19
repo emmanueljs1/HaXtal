a97fe11e472d646cfa542bf6506b547cabb34f50
Kyle Rosenbluth <kyle.rosenbluth@gmail.com>
2017-04-18 20:23:11 -0400

    Replace all old fractals

--- src/LSystem.hs
+++ src/LSystem.hs
@@ -15,9 +15,9 @@ type Rules = Map Symbol [Symbol]
 data LSystem = LSystem {start :: [Symbol], rules :: Rules, angle :: Double}
 
 expand :: LSystem -> [[Symbol]]
-expand (LSystem initial rules _) = iterate app initial
+expand (LSystem initial rs _) = iterate app initial
   where
-    app = concatMap (\s -> (findWithDefault [s] s rules))
+    app = concatMap (\s -> findWithDefault [s] s rs)
 
 toSymbol :: Char -> Symbol
 toSymbol 'F' = Forward
@@ -32,28 +32,22 @@ makeStart :: String -> [Symbol]
 makeStart = fmap toSymbol
 
 sierpinski' :: LSystem
-sierpinski' = LSystem (makeStart "FX") (r1 <> r2 <> r3) 60.0 where
+sierpinski' = LSystem (makeStart "FX") (r1 <> r2 <> r3) 60 where
   r1 = makeRule 'F' "Z"
   r2 = makeRule 'X' "+FY-FX-FY+"
   r3 = makeRule 'Y' "-FX+FY+FX-"
 
-sierpinski = LSystem [Forward, f, minus, Forward, g, minus, Forward, g] (fRule <> gRule <> zR1) 120.0 where
-  f = Var 'F'
-  g = Var 'G'
-  fRule = singleton f [Forward, f, minus, Forward, g, plus, Forward, f, plus, Forward, g, minus, Forward, f]
-  gRule = singleton g [Forward, g, Forward, g]
-  zR1 = singleton Forward [Var 'Z']
-  plus = RightTurn
-  minus = LeftTurn
+sierpinski :: LSystem
+sierpinski = LSystem (makeStart "FA-FG-FG") (r1 <> r2 <> r3) 120 where
+  r1 = makeRule 'A' "FA-FG+FA+FG-FA"
+  r2 = makeRule 'G' "FGFG"
+  r3 = makeRule 'F' "Z"
 
 dragon :: LSystem
-dragon = LSystem [Forward, x] (r1 <> r2) 90 where
-    x = Var 'X'
-    y = Var 'Y'
-    r1 = singleton x [x, plus, y, Forward, plus]
-    r2 = singleton y [minus, Forward, x, minus, y]
-    plus = RightTurn
-    minus = LeftTurn
+dragon = LSystem (makeStart "FX") (r1 <> r2) 90 where
+  r1 = makeRule 'X' "X+YF+"
+  r2 = makeRule 'Y' "-FX-Y"
+
 instance Arbitrary LSystem where
   arbitrary = undefined
   shrink = undefined
