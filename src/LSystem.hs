7cf3239f6ebf5db99a9659323ef82505c58d011c
davidbuckman <davidbuckman7@gmail.com>
2017-04-22 22:24:33 -0400

    Hackily do braces and rename to HaXtal

--- src/LSystem.hs
+++ src/LSystem.hs
@@ -8,19 +8,19 @@ import Data.Maybe
 import Control.Monad
 
 data Symbol = Forward
-            | LeftTurn
-            | RightTurn
+            | Turn Float
+            | PushState
+            | PopState
             deriving (Eq, Ord, Show)
 
 type Rules = Map Char String
 
 type DrawRules = Map Char Symbol
 
-data LSystem = LSystem {start :: String, rules :: Rules,
- toDraw :: DrawRules, angle :: Double}
+data LSystem = LSystem {start :: String, rules :: Rules, toDraw :: DrawRules}
 
 expand :: LSystem -> [[Symbol]]
-expand (LSystem initial rs td _) =
+expand (LSystem initial rs td) =
   mapMaybe (`lookup` td) <$> iterate app initial
   where
     app = concatMap (\s -> findWithDefault [s] s rs)
@@ -32,21 +32,28 @@ makeDrawRule = singleton
 fDrawRule :: DrawRules
 fDrawRule = makeDrawRule 'F' Forward
 
-plusDrawRule :: DrawRules
-plusDrawRule = makeDrawRule '+' RightTurn
+lBracketDrawRule :: DrawRules
+lBracketDrawRule = makeDrawRule '[' PushState
 
-minusDrawRule :: DrawRules
-minusDrawRule = makeDrawRule '-' LeftTurn
+rBracketDrawRule :: DrawRules
+rBracketDrawRule = makeDrawRule ']' PopState
 
-defaultDrawRules :: DrawRules
-defaultDrawRules = fDrawRule <> plusDrawRule <> minusDrawRule
+makePlusDrawRule :: Float -> DrawRules
+makePlusDrawRule a = makeDrawRule '+' (Turn a)
+
+makeMinusDrawRule :: Float -> DrawRules
+makeMinusDrawRule a = makeDrawRule '-' (Turn a)
+
+makeDefaultDrawRules :: Float -> DrawRules
+makeDefaultDrawRules a = fDrawRule <> lBracketDrawRule <> rBracketDrawRule <>
+                          makePlusDrawRule (2 * pi - a) <> makeMinusDrawRule a
 
 makeRule :: Char -> String -> Rules
 makeRule = singleton
 
 sierpinski :: LSystem
 sierpinski = LSystem "A-G-G" (r1 <> r2)
-             (defaultDrawRules <> dr1 <> dr2) 120 where
+             (makeDefaultDrawRules (2 * pi / 3) <> dr1 <> dr2) where
   r1 = makeRule 'A' "A-G+A+G-A"
   r2 = makeRule 'G' "GG"
   dr1 = makeDrawRule 'A' Forward
@@ -54,29 +61,34 @@ sierpinski = LSystem "A-G-G" (r1 <> r2)
 
 sierpinskiArrowhead :: LSystem
 sierpinskiArrowhead =
-  LSystem "A" (r1 <> r2) (defaultDrawRules <> dr1 <> dr2) 60 where
+  LSystem "A" (r1 <> r2) (makeDefaultDrawRules (pi / 3) <> dr1 <> dr2) where
   r1 = makeRule 'A' "+B-A-B+"
   r2 = makeRule 'B' "-A+B+A-"
   dr1 = makeDrawRule 'A' Forward
   dr2 = makeDrawRule 'B' Forward
 
 dragon :: LSystem
-dragon = LSystem "FX" (r1 <> r2) defaultDrawRules 90 where
+dragon = LSystem "FX" (r1 <> r2) (makeDefaultDrawRules (pi / 2)) where
   r1 = makeRule 'X' "X+YF+"
   r2 = makeRule 'Y' "-FX-Y"
 
 hilbert :: LSystem
-hilbert = LSystem "X" (r1 <> r2) defaultDrawRules 90 where
+hilbert = LSystem "X" (r1 <> r2) (makeDefaultDrawRules (pi / 2)) where
   r1 = makeRule 'X' "-YF+XFX+FY-"
   r2 = makeRule 'Y' "+XF-YFY-FX+"
 
 gosper :: LSystem
 gosper = LSystem "A" (r1 <> r2)
-         (defaultDrawRules <> dr1 <> dr2) 60 where
+         (makeDefaultDrawRules (pi / 3) <> dr1 <> dr2) where
   r1 = makeRule 'A' "A-B--B+A++AA+B-"
   r2 = makeRule 'B' "+A-BB--B-A++A+B"
   dr1 = makeDrawRule 'A' Forward
   dr2 = makeDrawRule 'B' Forward
+
+plant :: LSystem
+plant = LSystem "X" (r1 <> r2) (makeDefaultDrawRules (25 * pi / 180)) where
+  r1 = makeRule 'X' "F-[[X]+X]+F[+FX]-X"
+  r2 = makeRule 'F' "FF"
 
 instance Arbitrary LSystem where
   arbitrary = undefined
