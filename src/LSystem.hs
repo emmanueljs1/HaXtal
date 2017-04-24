36202be8f743fe2e302df81bd736ad19842f11a4
Kyle Rosenbluth <kyle.rosenbluth@gmail.com>
2017-04-24 16:32:14 -0400

    Pull examples out of LSystem.hs and refactor Main a bit

--- src/LSystem.hs
+++ src/LSystem.hs
@@ -20,6 +20,9 @@ type DrawRules = Map Char Symbol
 
 data LSystem = LSystem {start :: String, rules :: Rules, toDraw :: DrawRules}
 
+-- Takes an LSystem and produces an infinite list of iterative expansions.
+-- The nth element of the result is the list of Symbols obtained from
+-- expanding the LSystem n iterations.
 expand :: LSystem -> [[Symbol]]
 expand (LSystem initial rs td) =
   mapMaybe (`lookup` td) <$> iterate app initial
@@ -51,45 +54,6 @@ makeDefaultDrawRules a = fDrawRule <> lBracketDrawRule <> rBracketDrawRule <>
 
 makeRule :: Char -> String -> Rules
 makeRule = singleton
-
-sierpinski :: LSystem
-sierpinski = LSystem "A-G-G" (r1 <> r2)
-             (makeDefaultDrawRules (2 * pi / 3) <> dr1 <> dr2) where
-  r1 = makeRule 'A' "A-G+A+G-A"
-  r2 = makeRule 'G' "GG"
-  dr1 = makeDrawRule 'A' Forward
-  dr2 = makeDrawRule 'G' Forward
-
-sierpinskiArrowhead :: LSystem
-sierpinskiArrowhead =
-  LSystem "A" (r1 <> r2) (makeDefaultDrawRules (pi / 3) <> dr1 <> dr2) where
-  r1 = makeRule 'A' "+B-A-B+"
-  r2 = makeRule 'B' "-A+B+A-"
-  dr1 = makeDrawRule 'A' Forward
-  dr2 = makeDrawRule 'B' Forward
-
-dragon :: LSystem
-dragon = LSystem "FX" (r1 <> r2) (makeDefaultDrawRules (pi / 2)) where
-  r1 = makeRule 'X' "X+YF+"
-  r2 = makeRule 'Y' "-FX-Y"
-
-hilbert :: LSystem
-hilbert = LSystem "X" (r1 <> r2) (makeDefaultDrawRules (pi / 2)) where
-  r1 = makeRule 'X' "-YF+XFX+FY-"
-  r2 = makeRule 'Y' "+XF-YFY-FX+"
-
-gosper :: LSystem
-gosper = LSystem "A" (r1 <> r2)
-         (makeDefaultDrawRules (pi / 3) <> dr1 <> dr2) where
-  r1 = makeRule 'A' "A-B--B+A++AA+B-"
-  r2 = makeRule 'B' "+A-BB--B-A++A+B"
-  dr1 = makeDrawRule 'A' Forward
-  dr2 = makeDrawRule 'B' Forward
-
-plant :: LSystem
-plant = LSystem "X" (r1 <> r2) (makeDefaultDrawRules (25 * pi / 180)) where
-  r1 = makeRule 'X' "F-[[X]+X]+F[+FX]-X"
-  r2 = makeRule 'F' "FF"
 
 instance Arbitrary LSystem where
   arbitrary = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -5,32 +5,42 @@
 
 import Draw
 import LSystem
+import Examples
 import Reflex.Dom
 import Control.Monad.IO.Class
-import Control.Monad (foldM_)
 import qualified Data.Text as T
 import qualified Data.Map as Map
 import Data.JSString
 import Data.Monoid
-
-import qualified GHCJS.DOM.JSFFI.Generated.CanvasRenderingContext2D as D
+import Data.Foldable (traverse_)
+import qualified GHCJS.DOM.JSFFI.Generated.CanvasRenderingContext2D as CVS
 import qualified GHCJS.DOM.Types as DOM
-import qualified GHCJS.DOM.JSFFI.Generated.HTMLCanvasElement as CTX
+import qualified GHCJS.DOM.JSFFI.Generated.HTMLCanvasElement as CVS
+
 main :: IO ()
 main = mainWidget $ do
   el "h1" $ text "Welcome to HaXtal!"
   el "h2" $ text "Please select a fractal to display:"
+  dd <- dropdown 1 (constDyn $ (1 =: "Sierpinski") <> (2 =: "Gosper")) def
+  el "br" $ blank
   (e, _) <- element "canvas" def blank
   let canvas = DOM.HTMLCanvasElement $ DOM.unElement . DOM.toElement . _element_raw $ e
-  ctx' <- CTX.getContext canvas (pack "2d")
+  ctx' <- CVS.getContext canvas (pack "2d")
   let ctx = DOM.CanvasRenderingContext2D $ ctx'
-  CTX.setWidth canvas 1000
-  CTX.setHeight canvas 1000
+  CVS.setWidth canvas 1000
+  CVS.setHeight canvas 1000
   drawPaths (getPaths gosper 5) ctx
+  --traverse_ (flip drawPaths ctx) (getPaths . lsysFromDD $ value dd)
 
+lsysFromDD :: Int -> LSystem
+lsysFromDD 1 = sierpinski
+lsysFromDD 2 = gosper
+lsysFromDD _ = dragon
+
+-- Takes a list of a list of points an
 drawPaths :: (MonadIO m) => [[Vector]] -> DOM.CanvasRenderingContext2D -> m ()
-drawPaths paths ctx = D.beginPath ctx >> (foldM_ drawPath () paths) >> D.stroke ctx where
-  drawPath _ p@(p1:_) = do
+drawPaths paths ctx = CVS.beginPath ctx >> (traverse_ drawPath paths) >> CVS.stroke ctx where
+  drawPath p@(p1:_) = do
     let t v = map2Tuple (* 5.0) v
-    uncurry (D.moveTo ctx) $ t p1
-    foldM_ (\_ poi -> uncurry (D.lineTo ctx) $ t poi) () p
+    uncurry (CVS.moveTo ctx) $ t p1
+    traverse_ (\poi -> uncurry (CVS.lineTo ctx) $ t poi) p
