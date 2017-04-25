0d1955808b9bce107df3914d02e6a54a74e08f0d
Kyle Rosenbluth <kyle.rosenbluth@gmail.com>
2017-04-25 00:55:22 -0400

    Refactor main. Add dropdown to change what fractal is being displayed

--- src/Main.hs
+++ src/Main.hs
@@ -21,28 +21,54 @@ main :: IO ()
 main = mainWidget $ do
   el "h1" $ text "Welcome to HaXtal!"
   el "h2" $ text "Please select a fractal to display:"
-  dd <- dropdown 1 (constDyn $ (1 =: "Sierpinski") <> (2 =: "Gosper")) def
+  dd <- dropdown 1 ddOpts def
   el "br" blank
   (e, _) <- element "canvas" def blank
-  let canvas = DOM.HTMLCanvasElement $ DOM.unElement . DOM.toElement . _element_raw $ e
+  let canvas = DOM.HTMLCanvasElement $
+               DOM.unElement . DOM.toElement . _element_raw $ e
   ctx' <- CVS.getContext canvas (pack "2d")
   let ctx = DOM.CanvasRenderingContext2D ctx'
-  --drawPaths ctx . getPaths 5 . lsysFromDD <$> value dd
-  CVS.setWidth canvas 1000
-  CVS.setHeight canvas 1000
-  drawPaths ctx (getPaths 5 gosper)
-
-   -- drawPaths ctx . getPaths 5 . lsysFromDD <$> value dd
-
-lsysFromDD :: Integer -> LSystem
-lsysFromDD 1 = sierpinski
-lsysFromDD 2 = gosper
-lsysFromDD _ = dragon
+  CVS.setWidth canvas $ round canvasWidth
+  CVS.setHeight canvas $ round canvasHeight
+  -- Draw the default fractal from the starting selection of the dropdown
+  drawPaths ctx (getPaths defaultLevels $ lsysFromDD 1)
+  -- Attach the redrawing of fractals to the changing of the dropdown
+  performEvent_ $ liftIO . (drawPaths ctx) . getPaths defaultLevels . lsysFromDD
+               <$> (updated $ value dd)
+  return ()
 
 -- Takes a list of a list of points an
-drawPaths :: (MonadIO m) => DOM.CanvasRenderingContext2D -> [[Vector]] -> m ()
-drawPaths ctx paths = CVS.beginPath ctx >> traverse_ drawPath paths >> CVS.stroke ctx where
-  drawPath p@(p1:_) = do
-    let tr = map2Tuple (* 5.0)
-    uncurry (CVS.moveTo ctx) $ tr p1
-    traverse_ (uncurry (CVS.lineTo ctx) . tr) p
+drawPaths ::(MonadIO m) => DOM.CanvasRenderingContext2D -> [[Vector]] -> m ()
+drawPaths ctx paths = do
+  CVS.clearRect ctx 0 0 canvasWidth canvasHeight
+  CVS.save ctx
+  CVS.beginPath ctx
+  traverse_ drawPath paths
+  CVS.stroke ctx
+  CVS.restore ctx
+  where
+    drawPath p@(p1:_) = do
+      let tr = map2Tuple (\e -> e * drawingScale + canvasWidth / 2.0)
+      uncurry (CVS.moveTo ctx) $ tr p1
+      traverse_ (uncurry (CVS.lineTo ctx) . tr) p
+
+-------------- Helpers and Constants -------------------------------------------
+canvasWidth = 1000.0
+canvasHeight = 1000.0
+drawingScale = 10.0
+defaultLevels = 4
+ddOpts = (constDyn $ (1 =: "Gosper")
+                  <> (2 =: "Hilbert")
+                  <> (3 =: "Sierpinski")
+                  <> (4 =: "Dragon")
+                  <> (5 =: "Sierpinski Arrowhead")
+                  <> (6 =: "Plant"))
+lsysFromDD :: Integer -> LSystem
+lsysFromDD 1 = gosper
+lsysFromDD 2 = hilbert
+lsysFromDD 3 = sierpinski
+lsysFromDD 4 = dragon
+lsysFromDD 5 = sierpinskiArrowhead
+lsysFromDD 6 = plant
+lsysFromDD _ = dragon
+--------------------------------------------------------------------------------
