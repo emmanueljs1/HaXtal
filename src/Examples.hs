c83aa6f1dac7f769dbb04482d050ec34664e09cb
Emmanuel Suarez <emmanueljs1@gmail.com>
2017-04-24 21:36:13 -0400

    adding comments

--- src/Examples.hs
+++ src/Examples.hs
@@ -1,9 +1,16 @@
+-- | This file containns examples of common fractals represented as LSystems
+-- | It also gives the option of drawing any of these using Haskell's Gloss
+-- | library
+
 module Examples where
 import LSystem
 import Draw
 import Data.Monoid
 import Graphics.Gloss
+--------------------------------------------------------------------------------
+-- | Examples
 
+-- Sierpinski Fractal
 sierpinski :: LSystem
 sierpinski = LSystem "A-G-G" (r1 <> r2)
              (makeDefaultDrawRules (2 * pi / 3) <> dr1 <> dr2) where
@@ -12,6 +19,7 @@ sierpinski = LSystem "A-G-G" (r1 <> r2)
   dr1 = makeDrawRule 'A' Forward
   dr2 = makeDrawRule 'G' Forward
 
+-- Sierpinski Arrowhead Fractal
 sierpinskiArrowhead :: LSystem
 sierpinskiArrowhead =
   LSystem "A" (r1 <> r2) (makeDefaultDrawRules (pi / 3) <> dr1 <> dr2) where
@@ -20,16 +28,19 @@ sierpinskiArrowhead =
   dr1 = makeDrawRule 'A' Forward
   dr2 = makeDrawRule 'B' Forward
 
+-- Dragon Fractal
 dragon :: LSystem
 dragon = LSystem "FX" (r1 <> r2) (makeDefaultDrawRules (pi / 2)) where
   r1 = makeRule 'X' "X+YF+"
   r2 = makeRule 'Y' "-FX-Y"
 
+-- Hilbert Curve
 hilbert :: LSystem
 hilbert = LSystem "X" (r1 <> r2) (makeDefaultDrawRules (pi / 2)) where
   r1 = makeRule 'X' "-YF+XFX+FY-"
   r2 = makeRule 'Y' "+XF-YFY-FX+"
 
+-- Gosper Curve
 gosper :: LSystem
 gosper = LSystem "A" (r1 <> r2)
          (makeDefaultDrawRules (pi / 3) <> dr1 <> dr2) where
@@ -38,10 +49,14 @@ gosper = LSystem "A" (r1 <> r2)
   dr1 = makeDrawRule 'A' Forward
   dr2 = makeDrawRule 'B' Forward
 
+-- Plant Curve
 plant :: LSystem
 plant = LSystem "X" (r1 <> r2) (makeDefaultDrawRules (25 * pi / 180)) where
   r1 = makeRule 'X' "F-[[X]+X]+F[+FX]-X"
   r2 = makeRule 'F' "FF"
+
+--------------------------------------------------------------------------------
+-- | Code for drawing curves
 
 window :: Display
 window = InWindow "Nice Window" (200, 200) (10, 10)
--- src/LSystem.hs
+++ src/LSystem.hs
@@ -1,3 +1,6 @@
+-- | This file lays down a framework for representing LSystems
+-- | as Haskell Types
+
 module LSystem where
 
 import Prelude hiding (lookup)
@@ -7,17 +10,36 @@ import Data.Monoid
 import Data.Maybe
 import Control.Monad
 
-data Symbol = Forward
-            | Turn Float
-            | PushState
-            | PopState
-            | BOOP
+-- a symbol represents an action to be performed
+data Symbol = Forward     -- draw forward
+            | Turn Float  -- turn the current angle
+            | PushState   -- save the current position
+            | PopState    -- access the most recently pushed position
+            | BOOP        -- TODO: what is BOOP
             deriving (Eq, Ord, Show)
 
+{- in our framework, a _variable_ or a _constant_ can have a
+-- symbol associated with it
+
+-- the difference between a variable and a constant is that a variable
+-- has a rule that defines how it is replaced, while a constant does not -}
+
+-- rules define the way a variable can be replaced with a combination of
+-- variables and constants
+-- we represent them as a map from characters to strings
+-- for example X -> XX could be represented in the map as ('X', "XX")
 type Rules = Map Char String
 
+-- draw rules map variables and constants to symbols
 type DrawRules = Map Char Symbol
 
+-- finally, an LSystem is a start state, a set of rules, and a set of draw rules
+-- an example LSystem could be:
+--   variables: F
+--   constants: +
+--   start: F
+--   rules: F -> F + F
+--   draw rules: F -> Forward; + -> Turn 90
 data LSystem = LSystem {start :: String, rules :: Rules, toDraw :: DrawRules}
 
 -- Takes an LSystem and produces an infinite list of iterative expansions.
@@ -29,31 +51,56 @@ expand (LSystem initial rs td) =
   where
     app = concatMap (\s -> findWithDefault [s] s rs)
 
+-- base draw rules (no rules at all)
+baseDrawRule :: DrawRules
+baseDrawRule = empty
 
+-- makes a draw rule (associates a character to a symbol)
 makeDrawRule :: Char -> Symbol -> DrawRules
 makeDrawRule = singleton
 
+-- combines two sets of draw rules into one set of draw rules
+combineDrawRules :: DrawRules -> DrawRules -> DrawRules
+combineDrawRules = (<>)
+
+-- forward draw rule, relates 'F' to Forward
 fDrawRule :: DrawRules
 fDrawRule = makeDrawRule 'F' Forward
 
+-- push state draw rule, relates '[' to saving state
 lBracketDrawRule :: DrawRules
 lBracketDrawRule = makeDrawRule '[' PushState
 
+-- pop state draw rule, relates ']' to popping most recent state
 rBracketDrawRule :: DrawRules
 rBracketDrawRule = makeDrawRule ']' PopState
 
+-- plus draw rule, relates '+' to turning by an angle a
 makePlusDrawRule :: Float -> DrawRules
 makePlusDrawRule a = makeDrawRule '+' (Turn a)
 
+-- minus draw rule, relates '-' to turning by an angle a
 makeMinusDrawRule :: Float -> DrawRules
 makeMinusDrawRule a = makeDrawRule '-' (Turn a)
 
+-- default draw rules are the most common rules, these relate
+-- 'F' to Forward, '[' to push state, ']' to pop state, '+' and '-' to turning
+-- by complementary angles
 makeDefaultDrawRules :: Float -> DrawRules
 makeDefaultDrawRules a = fDrawRule <> lBracketDrawRule <> rBracketDrawRule <>
                           makePlusDrawRule (2 * pi - a) <> makeMinusDrawRule a
 
+-- base rule (no rules)
+baseRule :: Rules
+baseRule = empty
+
+-- makes a rule
 makeRule :: Char -> String -> Rules
 makeRule = singleton
+
+-- combine two sets of rules into one set of rules
+combineRules :: Rules -> Rules -> Rules
+combineRules = (<>)
 
 instance Arbitrary LSystem where
   arbitrary = undefined
