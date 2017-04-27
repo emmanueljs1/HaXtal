a51487fefe25aae7f2fb24afbe7561af17780ca9
Emmanuel Suarez <emmanueljs1@gmail.com>
2017-04-27 14:25:48 -0400

    main menu, fixed getLSystem, updated arbitrary, LSystemTest

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,8 @@ import Examples
 import LSystem
 import Draw
 import Test.QuickCheck
+import Text.Read
+import Data.Maybe
 
 -- | Code for drawing curves
 toGlossPoint :: Draw.Point -> Graphics.Gloss.Point
@@ -13,7 +15,7 @@ toGlossPoint p = (getX p, getY p)
 -- draws a Picture from an LSystem
 drawPicture :: Int -> LSystem -> Picture
 drawPicture depth lsys =
-  pictures (line <$> (toGlossPoint <$>) <$> getPaths depth lsys)
+  pictures (line . (toGlossPoint <$>) <$> getPaths depth lsys)
 
 window :: Display
 window = InWindow "LSystems!" (200, 200) (10, 10)
@@ -49,9 +51,18 @@ drawingKochLake :: Picture
 drawingKochLake = drawPicture 3 kochLake
 
 main :: IO ()
-main = display window background drawingKochLake
--- main = do
---   lsys <- generate arbitrary
---   print lsys
---   print $ getDrawBounds (concat $ getPaths 6 lsys)
---   display window background (drawPicture 6 lsys)
+main = do
+   putStrLn "Pick a Random LSystem Generator or a pregenerated LSystem"
+   putStrLn "0: can use all constants"
+   putStrLn "1: only turns"
+   putStrLn "2: turns and pushes/pops"
+   putStrLn "3: turns and angle increases/decreases"
+   s <- getLine
+   let op = fromMaybe 0 (readMaybe s)
+   lsys <- generate (resize op arbitrary)
+   let pic =
+     if op < 4 then
+       drawPicture 6 lsys
+     else
+       drawingSierpinski
+   display window background pic
